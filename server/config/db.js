8d2e514a618ecea9034337e3f7ebe263a5322c06
roelma2000 <roelma2000@gmail.com>
2022-03-02 11:41:30 -0500

    Modified Button Text
    Changed  "Add User" to "Add Contacts"

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,6 @@
 //  Date: 2022-Feb-26
 
 module.exports={
-    //"URI" : "mongodb://127.0.0.1:27018/ExpressAuthDB"
+        //"URI" : "mongodb://127.0.0.1:27018/ExpressAuthDB"
     "URI" : "mongodb+srv://mongoUser:AmnpPM83go0zAhEv@comp229expressportfolio.k2idq.mongodb.net/ExpressAuthDB?retryWrites=true&w=majority"
 }
